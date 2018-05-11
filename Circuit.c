94d746e95cacc12a56579450c51c4af272d9d5af
max0218 <36679390+max0218@users.noreply.github.com>
2018-05-12 00:43:14 +0200

    Exercice 5 à tester

--- Circuit.c
+++ Circuit.c
@@ -8,32 +8,78 @@
 #include "Circuit.h"
 
 
+//Initialise un Cell_circuit
+Cell_circuit* CC_Init(int jmin,int jmax){
+
+	Cell_circuit *cc=malloc(sizeof(Cell_circuit));
+
+	cc->L.premier=NULL;
+	cc->L.dernier=NULL;
+
+	cc->jmin=jmin;
+	cc->jmax=jmax;
+
+	cc->suiv=NULL;
+
+	return cc;
+}
+
+//Initialise un LCircuit
+Lcircuit* Lcircuit_Init(){
+
+	Lcircuit* LC=malloc(sizeof(Lcircuit));
+
+	LC->nb_circuit=0;
+	LC->premier=NULL;
+	LC->dernier=NULL;
+
+	return LC;
+}
+
+//Insertion en queue dans un Lcircuit
+Insertion_en_queue_Lcircuit(Lcircuit *LC,Cell_circuit* cc){
+
+	if(LC->premier==NULL){
+		LC->premier=cc;
+		LC->dernier=cc;
+	}else{
+		LC->dernier->suiv=cc;
+		LC->dernier=cc;
+		cc->suiv=NULL;
+	}
+	LC->nb_circuit++;
+}
+
+//Recherche d'un circuit à partir d'un sommet 
+Cell_circuit* Sommet_Rech_Circuit(Graphe* H,Sommet* somm){
+
+	Sommet* psom=sommet;
+	Cell_circuit* c;
+
+	while(psom->visit!=0){
+		LDCInsererEnFin(c->L,psom->i,psom->j);
+		psom->visit=0;
+
+		psom=psom->Lsucc->succ;
+	}
+	LDCafficher(c->L);
+	printf("Fin du circuit \n");
+}
+
 //Permet d'afficher un ensemble de circuits couvrant les sommets non-noirs de H
 void Graphe_Rech_Circuit(Graphe *H){
 	int i,j;
-	Sommet *scour;
-	Arc *acour;
+	//Sommet *scour;
+	//Arc *acour;
+	//On pourrait réduire le nombre d'itérations 
 	for(i=0;i<H->n;i++){
 		
 		for(j=0;j<H->m;j++){
 
 			if(H->Tsom[i][j]->visit==-1){
+			
+				Sommet_Rech_Circuit(H,H->TSom[i][j]); // Affiche un circuit 
 
-				printf("\n(%d,%d) ",i,j);
-				H->Tsom[i][j]->visit=0;
-				scour=H->Tsom[i][j];
-				do{
-					acour=scour->Lsucc;
-					while((acour!=NULL)&&(acour->succ!=H->Tsom[i][j])&&(acour->succ->visit != -1)){
-						acour=acour->suiv;
-					}
-					if(acour!=NULL){
-						scour=acour->succ;
-						scour->visit=0;
-						printf("(%d,%d) ",scour->i,scour->j);
-					}
-				}while (scour!=H->Tsom[i][j]);
-				printf("\n");
 			}
 		}
 	}
@@ -43,4 +89,18 @@ void Graphe_Rech_Circuit(Graphe *H){
 //Retourne une liste de circuits de H couvrant les sommets non-noirs
 void Graphe_Rech_Circuit_v2(Graphe *H, Lcircuit *LC){
 
+	for(i=0;i<H->n;i++){
+		
+		for(j=0;j<H->m;j++){
+
+			if(H->Tsom[i][j]->visit==-1){
+
+				cell_circuit *cc=CC_init(0,0);		//Initialisation d'une liste chainée de circuit
+
+				cc=Sommet_Rech_Circuit(H,H->Tsom[i][j]);	//Un circuit de sommets non-noirs
+
+				Insertion_en_queue_Lcircuit(LC,cc); //Insertion du circuit dans une liste
+			}
+		}
+	}
 }
--- Circuit.h
+++ Circuit.h
@@ -16,6 +16,16 @@ Cell_circuit *premier;
 Cell_circuit *dernier;
 } Lcircuit;
 
+
+//Initialise un Cell_circuit
+Cell_circuit* CC_Init(int jmin,int jmax);
+
+//Initialise un LCircuit
+Lcircuit* Lcircuit_Init();
+
+//Insertion en queue dans un Lcircuit
+Insertion_en_queue_Lcircuit(Lcircuit *LC,Cell_circuit* cc);
+
 //Permet d'afficher un ensemble de circuits couvrant les sommets non-noirs de H
 void Graphe_Rech_Circuit(Graphe *H);
 
